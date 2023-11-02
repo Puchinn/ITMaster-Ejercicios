9ca85c8dca5b42faf693e2f2ccd21b695fb6d34c
Esteban Sayago <sayagoo823@gmail.com>
2023-11-01 23:27:34 -0300

    actualizar nombre de variables y mover codigo

--- tramo-3/04-filtros-async-await/20-ejercicio/index.js
+++ tramo-3/04-filtros-async-await/20-ejercicio/index.js
@@ -3,67 +3,66 @@ const lista = document.querySelector("ul")
 
 let listaLibros = []
 
-function guardarLibro (e){
+formulario.addEventListener("submit", guardarLibro)
+document.addEventListener("DOMContentLoaded", renderizarLibros)
+
+function renderizarLibros() {
+  cargarLibros()
+  listaLibros.forEach(libro => {
+    const tituloSinEspacios = libro.titulo.replace(/\s/gi, "-")
+    lista.innerHTML += `
+    <li id=${tituloSinEspacios}>
+      <details>
+        <summary> ${libro.titulo} </summary>
+        <p>Autor: ${libro.autor}</p>
+        <p>Fecha de publicación: ${libro.anho}</p>
+        <button onclick=eliminarLibro("${tituloSinEspacios}") >Eliminar</button>
+      </details>
+    </li>
+    `
+  })
+}
+
+function guardarLibro(e) {
   e.preventDefault()
   const formValues = e.target
   const titulo = formValues.titulo.value
   const autor = formValues.autor.value
   const anho = formValues.anho.value
 
-  if(titulo.length > 1 && autor.length > 1 && anho.length > 1){
-    const libro = {
-      titulo,
-      autor,
-      anho
-    }
-
-    listaLibros.push(libro)
-    window.localStorage.setItem("lista-libros",JSON.stringify(listaLibros))
+  if (titulo.length > 1 && autor.length > 1 && anho.length > 1) {
+    const libro = { titulo, autor, anho }
+    const tituloSinEspacios = libro.titulo.replace(/\s/gi, "-")
     lista.innerHTML += `
-    <li id=${libro.titulo.replace(/\s/gi,"-")}>
+    <li id=${tituloSinEspacios}>
       <details>
         <summary> ${libro.titulo} </summary>
         <p>Autor: ${libro.autor}</p>
         <p>Fecha de publicación: ${libro.anho}</p>
-        <button onclick={eliminarLibro("${libro.titulo.replace(/\s/gi,"-")}")}>Eliminar</button>
+        <button onclick={eliminarLibro("${tituloSinEspacios}")}>Eliminar</button>
       </details>
     </li>
     `
-  }else{
+    listaLibros.push(libro)
+    window.localStorage.setItem("lista-libros", JSON.stringify(listaLibros))
+  } else {
     alert("Completar todos los campos")
   }
 }
 
-function cargarLibros(){
+function cargarLibros() {
   const libros = JSON.parse(window.localStorage.getItem("lista-libros"))
-  if(libros){
+  if (libros) {
     listaLibros = libros
   }
 }
 
-function eliminarLibro(titulo){
+function eliminarLibro(titulo) {
   cargarLibros()
-  const nuevaLista = listaLibros.filter(l => l.titulo !== titulo.replace(/-/gi," "))
-  window.localStorage.setItem("lista-libros",JSON.stringify(nuevaLista))
+  const tituloOriginal = titulo.replace(/-/gi, " ")
+  const nuevaLista = listaLibros.filter(l => l.titulo !== tituloOriginal)
+  window.localStorage.setItem("lista-libros", JSON.stringify(nuevaLista))
   document.getElementById(titulo).remove()
   listaLibros = nuevaLista
 }
-
-formulario.addEventListener("submit",guardarLibro)
-document.addEventListener("DOMContentLoaded",()=>{
-  cargarLibros()
-  listaLibros.forEach(libro => {
-    const titulo = libro.titulo.replace(/\s/gi,"-")
-    lista.innerHTML += `
-    <li id=${titulo}>
-      <details>
-        <summary> ${libro.titulo} </summary>
-        <p>Autor: ${libro.autor}</p>
-        <p>Fecha de publicación: ${libro.anho}</p>
-        <button onclick=eliminarLibro("${titulo}") >Eliminar</button>
-      </details>
-    </li>
-    `
-  })
-})
 
