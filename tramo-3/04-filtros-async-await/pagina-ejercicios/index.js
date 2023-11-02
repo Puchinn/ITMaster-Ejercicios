a5f6ceee4be92e46b30d5d9dc5386b56506cbd8b
Esteban Sayago <sayagoo823@gmail.com>
2023-11-01 23:35:01 -0300

    agregar el ejercicio 20

--- tramo-3/04-filtros-async-await/pagina-ejercicios/index.js
+++ tramo-3/04-filtros-async-await/pagina-ejercicios/index.js
@@ -71,3 +71,74 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.getElementById("content-19").innerHTML = contenido
   })
 })
+
+
+// Ejercicio 20
+const formulario_20 = document.getElementById("form-20")
+const lista_20 = document.getElementById("lista-20")
+
+let listaLibros = []
+
+formulario_20.addEventListener("submit", guardarLibro)
+document.addEventListener("DOMContentLoaded", renderizarLibros)
+
+function renderizarLibros() {
+  cargarLibros()
+  listaLibros.forEach(libro => {
+    const tituloSinEspacios = libro.titulo.replace(/\s/gi, "-")
+    lista_20.innerHTML += `
+    <li id=${tituloSinEspacios}>
+      <details>
+        <summary> ${libro.titulo} - ver detalles</summary>
+        <p>Autor: ${libro.autor}</p>
+        <p>Fecha de publicación: ${libro.anho}</p>
+        <button onclick=eliminarLibro("${tituloSinEspacios}") >Eliminar</button>
+      </details>
+    </li>
+    `
+  })
+}
+
+function guardarLibro(e) {
+  e.preventDefault()
+  const formValues = e.target
+  const titulo = formValues.titulo.value
+  const autor = formValues.autor.value
+  const anho = formValues.anho.value
+
+  if (titulo.length > 1 && autor.length > 1 && anho.length > 1) {
+    const libro = { titulo, autor, anho }
+    const tituloSinEspacios = libro.titulo.replace(/\s/gi, "-")
+    lista_20.innerHTML += `
+    <li id=${tituloSinEspacios}>
+      <details>
+        <summary> ${libro.titulo} </summary>
+        <p>Autor: ${libro.autor}</p>
+        <p>Fecha de publicación: ${libro.anho}</p>
+        <button onclick={eliminarLibro("${tituloSinEspacios}")}>Eliminar</button>
+      </details>
+    </li>
+    `
+    listaLibros.push(libro)
+    window.localStorage.setItem("lista-libros", JSON.stringify(listaLibros))
+  } else {
+    alert("Completar todos los campos")
+  }
+}
+
+function cargarLibros() {
+  const libros = JSON.parse(window.localStorage.getItem("lista-libros"))
+  if (libros) {
+    listaLibros = libros
+  }
+}
+
+function eliminarLibro(titulo) {
+  cargarLibros()
+  const tituloOriginal = titulo.replace(/-/gi, " ")
+  const nuevaLista = listaLibros.filter(l => l.titulo !== tituloOriginal)
+  window.localStorage.setItem("lista-libros", JSON.stringify(nuevaLista))
+  document.getElementById(titulo).remove()
+  listaLibros = nuevaLista
+}
+
